81c1c2edb90182d476eec30b91c3af859c3919cb
drunk-particles <adnanbr786@gmail.com>
2026-06-07 03:39:58 +0600

    sync: 2026-06-07

--- config/hypr/assets/hyprlock/battery-status.sh
+++ config/hypr/assets/hyprlock/battery-status.sh
@@ -7,7 +7,7 @@ battery_percentage=$(cat /sys/class/power_supply/BAT0/capacity)
 battery_status=$(cat /sys/class/power_supply/BAT0/status)
 
 # Define horizontal battery icons for each 10% segment
-battery_icons=("" "" "" "" "" "" "" "" "" "")
+battery_icons=("󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󰁹")
 
 # Define the charging icon
 charging_icon=""
@@ -24,4 +24,4 @@ if [ "$battery_status" = "Charging" ]; then
 fi
 
 # Output the battery icon (without percentage)
-echo "$battery_icon ${PCT}%"
+echo "$battery_icon ${PCT}"
