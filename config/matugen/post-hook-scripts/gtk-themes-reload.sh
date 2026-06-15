c3d3470242178bc4f9331d2ec711b6c176f0cc15
drunk-particles <adnanbr786@gmail.com>
2026-06-15 06:51:40 +0600

    sync: 2026-06-15

--- config/matugen/post-hook-scripts/gtk-themes-reload.sh
+++ config/matugen/post-hook-scripts/gtk-themes-reload.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
-# Force GTK3 theme reload
-gsettings set org.gnome.desktop.interface gtk-theme ""
-gsettings set org.gnome.desktop.interface gtk-theme "adw-gtk3-dark"
-
 # Force GTK4 theme reload
 gsettings set org.gnome.desktop.interface color-scheme prefer-light
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark
